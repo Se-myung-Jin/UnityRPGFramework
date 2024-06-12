17550974098ea477d4e8359115374181d8102123
Se-myung-Jin <tpaud94@naver.com>
2024-06-13 01:07:38 +0900

    add GeneralManager, InputManager

---
+++ RPGFramework/Assets/Scripts/Manager/GeneralManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralManager : MonoBehaviour
+{
+    static GeneralManager _instance;
+    static GeneralManager Instance {  get { Init(); return _instance; } }
+
+    InputManager _input = new InputManager();
+    public static InputManager Input { get { return Instance._input; } }
+    
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _input.OnUpdate();
+    }
+
+    static void Init()
+    {
+        if (_instance == null)
+        {
+            GameObject go = GameObject.Find("@GeneralManager");
+            if (go == null)
+            {
+                go = new GameObject { name = "@GeneralManager" };
+                go.AddComponent<GeneralManager>();
+            }
+
+            DontDestroyOnLoad(go);
+            _instance = go.GetComponent<GeneralManager>();
+        }
+    }
+}
