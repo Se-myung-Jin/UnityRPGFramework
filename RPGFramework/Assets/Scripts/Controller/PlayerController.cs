8f5f34133fa2fc81d69a5bed10aad81225d58c28
Se-myung-Jin <tpaud94@naver.com>
2024-06-15 09:12:11 +0900

    add camera controller and move to raycast

--- RPGFramework/Assets/Scripts/Controller/PlayerController.cs
+++ RPGFramework/Assets/Scripts/Controller/PlayerController.cs
@@ -7,14 +7,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
 
+    bool _moveToDest = false;
+    Vector3 _destPos;
+
     void Start()
     {
         GeneralManager.Input.KeyAction += OnKeyboard;
+        GeneralManager.Input.MouseAction += OnMouseClicked;
     }
 
     void Update()
     {
-        
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                transform.position += dir.normalized * moveDist;
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+            }
+        }
     }
     
     void OnKeyboard()
@@ -31,5 +49,18 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             //transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
             transform.Translate(Vector3.right * Time.deltaTime * _speed);
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
+        }
     }
 }
