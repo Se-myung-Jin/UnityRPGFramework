977646747caeb864caf49a35d066c03cfa2923f8
Se-myung-Jin <tpaud94@naver.com>
2024-06-20 00:01:21 +0900

    add UI Event Handler

---
+++ RPGFramework/Assets/Scripts/Util/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public static class Extension
+{
+    public static void AddUIEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+        UI_Base.AddUIEvent(go, action, type);
+    }
+}
